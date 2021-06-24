517b8a2495180db6e23a4e2b23fd56d4cdcc7f35
Sequeira8 <carlos.m.r.sequeira@gmail.com>
2021-06-24 18:51:21 +0100

    Update proj1.r

--- proj1.r
+++ proj1.r
@@ -1,7 +1,3 @@
-# install needed packages
-# install.packages("astsa")
-# install.packages("")
-
 #Import libraries
 library(readxl)
 library(ggplot2)
@@ -20,71 +16,6 @@ library(fBasics)
 library(zoo)
 library(lmtest) 
 library(forecast)
-
-
-setwd("C:\\Users\\carli\\Documents\\GitHub\\TimeSeries")
-
-# Values by hour, between 01-2014 and 12-2019
-data = read_excel("2014-2019 PM10 LisAvLib.xlsx")
-data$Data = as.Date(data$Data)
-
-names(data)[names(data) == "Data"] <- "date"
-names(data)[names(data) == "Av.da Lib. (�g/m3)"] <- "value"
-
-dim(data)
-
-
-# Analyse by day
-data = aggregate(data$value, by=list(data$date), function(x) mean(x, na.rm=TRUE))
-
-names(data)[names(data) == "Group.1"] <- "date"
-names(data)[names(data) == "x"] <- "value"
-
-data$value[is.nan(data$value)]<-NA
-
-data$value = na.approx(data$value)
-
-
-
-#Plots
-
-# Frequency = 12 since seasonal patterns repeat every year in the case of pm10 particles
-tsdata = ts(data$value, frequency = 12, start = c(2014, 1), end = c(2019,12))
-plot.ts(tsdata, ylab = 'PM10 particles (�g/m3)', xlab = 'Year')
-
-monthNames = c("Ja", "F", "Mr", "Ap", "Ma", "Jn", "Jl", "Au", "S", "O", "N", "D") 
-
-# Stl method to decompose into trend, season, and remainder
-y=stl(tsdata, s.window="period")
-y2=stlplus(tsdata, s.window="period", sub.labels = monthNames)
-plot(y)
-plot(y2)
-residuals=y$time.series[,3]
-
-# Partial autocorrelation function
-pacf(tsdata, lag.max=100, main="Partial Autocorrelation Original Data")
-
-acf(tsdata, lag.max=100, main="Autocorrelation Original Data")
-acf(residuals, lag.max=100, main="Autocorrelation Residuals")
-
-z=ts(tsdata)
-
-extremogram1(z, 0.90, 40, type=1, ploting = 1, cutoff = 1, start = 0)
-
-#Analisar gráfico do STL para análise exploratória?
-#modelo possível com os residuos 
-
-#Quais transformações? Nós só mudamos formato de data
-#transformar em dados diários - não de 24h!!. para cada dia!!
-#imputação?? interpolação linear, nearest neighboor
-#remover sazonalizade, trend, deterministicas
-
-
-#Report - Apresentação de resultados ou tbm parte mais teórica?
-#metodologia teórica, mesmo que sem muitos pormenores. 2 paginas com métodos e dps apresentação de resultados
-
-#faz sentido analisar por dia, semana, mês? fazer a média para cada um destes periodos?
-
 
 
 ####################################################################
@@ -110,29 +41,7 @@ acf(abs(log_returns), main="Autocorrelation Absolute Log Returns")
 
 
 #Model fitting
-
-if(FALSE){
-  for(x in 1:5){
-    for(y in 1:5){
-      print(typeof(x))
-      summary(garchFit(substitute(???garch(p,q), list(p=x, q=y)), data=log_returns, trace = FALSE))
-      print(x)
-      print(y)
-    }
-  }
-}
-fit = garchFit(???garch(1,1), data=log_returns, trace = FALSE)
-summary(garchFit(???garch(1,1), data=log_returns, trace = FALSE))
-
-predict(fit, n.ahead = 90, trace = FALSE, plot=TRUE)
-
-
-
 garch11.t.spec=ugarchspec(variance.model=list(garchOrder=c(1,1)), mean.model=list(armaOrder=c(0,0)), distribution.model = "std")
 garch11.t.fit=ugarchfit(spec=garch11.t.spec, data=log_returns)
 garch11.t.fit
 plot(garch11.t.fit, which="all")
-
-f=ugarchforecast(garch11.t.fit, n.ahead=20)
-f
-plot(f)
